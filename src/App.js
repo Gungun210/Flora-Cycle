4b56f34ed2cc832bfca0ef13c8c0867da7667b3d
srija <srija@srijas-MacBook-Air.local>
2026-03-18 21:07:31 +0530

    Added React Navbar and Footer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Petals from "./Petals";
+
+function App() {
+  return (
+    <div>
+      <Petals />
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
